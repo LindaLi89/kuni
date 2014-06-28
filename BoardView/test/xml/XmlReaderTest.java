926a88b9f012b4416fb39adc174290900a9b2425
Earnw1ng <earnwing@gmail.com>
2014-06-28 07:55:16 +0200

    test commit for eclipse git plugin

--- BoardView/test/xml/XmlReaderTest.java
+++ BoardView/test/xml/XmlReaderTest.java
@@ -7,27 +7,27 @@ import org.junit.Test;
 
 
 
-public class XmlReaderTest {
+public class XmlReaderTest { 
 
 	@Test
 	public void testReadXml() {
-		String path = "src/xml/XML_Beispiel_Daten/MiniWorld.xml";
-		
+		final String filePath = "src/xml/XML_Beispiel_Daten/MiniWorld.xml";
+
 		// assume empty elements (would be overwritten otherwise)
-		WorldData world = new WorldData();
-		world.setName("test world");
+		final WorldData worldData = new WorldData();
+		worldData.setName("test world");
 		/**
 		 * Obstacles are specified to be Extensions of the abstract class
 		 * "AbstractObject", which is not implemented. Instead we've got
 		 * constructors with super()-calls, although, the super class does
 		 * not exist.
-		 */
-		Obstacles leafs = new Obstacles("leaf", "Blatt"); //@todo
-		Obstacles shrooms = new Obstacles("mushroom", "Pilz"); //@todo
-		Obstacles trees = new Obstacles ("trees", "Baum"); //@todo
-		
+		 */ 
+		final Obstacles leafs = new Obstacles("leaf", "Blatt"); //@todo
+		final Obstacles shrooms = new Obstacles("mushroom", "Pilz"); //@todo
+		final Obstacles trees = new Obstacles ("trees", "Baum"); //@todo
+		  
 		assertNull(leafs.getObstacleDistribution());
-		Point point = XmlReader.readXML(path, world, leafs, shrooms, trees);
+		Point point = XmlReader.readXML(filePath, worldData, leafs, shrooms, trees);
 		assertNotNull(leafs.getObstacleDistribution());
 
 	}
